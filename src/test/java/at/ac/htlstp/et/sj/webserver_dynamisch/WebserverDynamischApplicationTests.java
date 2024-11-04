081e0284cfcf8046b23ad63b51e3fed471cd49d6
Leander Klik <160396774+LEGOlas06@users.noreply.github.com>
2024-11-04 16:34:58 +0100

    Ende der Stunde

--- src/test/java/at/ac/htlstp/et/sj/webserver_dynamisch/WebserverDynamischApplicationTests.java
+++ src/test/java/at/ac/htlstp/et/sj/webserver_dynamisch/WebserverDynamischApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebserverDynamischApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
