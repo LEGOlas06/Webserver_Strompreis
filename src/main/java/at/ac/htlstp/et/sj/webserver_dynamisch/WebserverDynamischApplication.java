21d339a8b2737a81feb7d08ed858bbb7f976364e
Leander Klik <160396774+LEGOlas06@users.noreply.github.com>
2024-11-04 15:26:54 +0100

    Initial commit

---
+++ src/main/java/at/ac/htlstp/et/sj/webserver_dynamisch/WebserverDynamischApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj.webserver_dynamisch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverDynamischApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverDynamischApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/htlstp/et/sj/webserver_dynamisch/WebserverDynamischApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.et.sj.webserver_dynamisch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverDynamischApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
